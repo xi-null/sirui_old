9749afa0df16e77ada1ca358012be93e2d7bca12
linzhichao <1315021555@qq.com>
2023-10-29 11:11:20 +0800

    翻新赛事回放和赛事集锦详情页面

--- pages/cutDetail/cutDetail.vue
+++ pages/cutDetail/cutDetail.vue
@@ -12,8 +12,8 @@
 			}
 		},
 		onLoad(option){
-			console.log(option)
-			this.src=`https://h5-1259813764.cos.ap-nanjing.myqcloud.com/cutDetail_2.html?orderId=${option.orderId}&userId=${option.userId}`
+			console.log('option',option)
+			this.src=`https://h5-1259813764.cos.ap-nanjing.myqcloud.com/replay_new_1.html?orderId=${option.orderId}&userId=${option.userId}`
 			console.log(this.src)
 		},
 	
---
+++ pages/highLight/highLight.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<!-- 赛事集锦页面 -->
+		 <web-view v-if="src" :src="src"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src:''
+			}
+		},
+		onLoad(option){
+			console.log('option',option)
+			// this.src=`https://h5-1259813764.cos.ap-nanjing.myqcloud.com/cutDetail_2.html?orderId=${option.orderId}&userId=${option.userId}`
+			this.src=`https://h5-1259813764.cos.ap-nanjing.myqcloud.com/highLight_new_1.html?orderId=${option.orderId}&userId=${option.userId}&tabIndex=${option.tabIndex}&vIndex=${option.vIndex}`
+			console.log(this.src)
+		},
+	
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
